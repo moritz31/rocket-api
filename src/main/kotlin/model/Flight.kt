add7b90929abf5d60009d8407af87f28a81c3749
Moritz <moritz.weichert@t-online.de>
2018-02-22 19:32:11 +0100

    Added forgotten file

---
+++ src/main/kotlin/model/Flight.kt
@@ -0,0 +1,42 @@
+package model
+
+import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object Flight : IntIdTable() {
+
+    val name = varchar("name", 50)
+
+    val time = datetime("time")
+
+    val location = reference("location",Site.id)
+
+    val vehicle = reference("vehicle",Vehicle.id)
+
+    val success = bool("success")
+}
+
+data class flight(val name: String, val time: String, val location: Int, val vehicle: Int, val success: Boolean)
+
+class FlightDAO() {
+
+    val flights: HashMap<Int,flight> = hashMapOf()
+
+    init {
+        transaction {
+            Flight.selectAll().forEach {
+                flights.put(it[Flight.id].value,
+                        flight(it[Flight.name].toString(),
+                                it[Flight.time].toString(),
+                                it[Flight.location].value,
+                                it[Flight.vehicle].value,
+                                it[Flight.success]))
+            }
+        }
+    }
+
+    fun findById(id: Int): flight? {
+        return flights[id]
+    }
+}
